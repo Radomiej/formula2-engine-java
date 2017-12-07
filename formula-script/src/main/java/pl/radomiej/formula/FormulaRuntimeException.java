3aadb2d1e0759e9dc71dd0dc827b5858e5b1ce6a
Radomiej <radek@fpsystem.pl>
2017-12-07 23:11:47 +0100

    add FormulaRuntimeException

---
+++ formula-script/src/main/java/pl/radomiej/formula/FormulaRuntimeException.java
@@ -0,0 +1,13 @@
+package pl.radomiej.formula;
+
+public class FormulaRuntimeException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8788780155813434948L;
+
+	public FormulaRuntimeException(String errorMessage) {
+		super(errorMessage);
+	}
+}
